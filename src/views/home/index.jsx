022a2b2ea1b5f973eff7abf7c2793bc4de2af517
debjitgill <debjit@gillbroking.com>
2023-08-28 14:54:53 +0530

    tailwing added

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <p>Home Page Works</p>
+      <p className='font-bold text-center'>Home Page Worksffff</p>
     </div>
   )
 }
